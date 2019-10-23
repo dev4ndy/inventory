255eb09906204611265323973bf7cf2a0245b015
Andres David Echeverri Jimenez <andrex.125d@gmail.com>
2019-10-22 19:12:44 -0500

    - Endpoint para obtener las bodegas. - Solución de error al hacer una transferencia. - Remover controlador de item. - Añadir ruta para obtener las bodegas.

--- lib/controllers/item.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export class ItemController {
-    public index(req: Request, res: Response) {
-        res.json({
-            message: 'Lista',
-        });
-    }
-}
